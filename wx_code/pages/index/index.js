2a80c02097f544f0cf4b8bdd146961a60de6e8c6
yingjiechen <YingjieChen@live.cn>
2017-10-20 10:11:21 +0800

    Project init Permission finished.

---
+++ wx_code/pages/index/index.js
@@ -0,0 +1,76 @@
+//index.js
+//获取应用实例
+const app = getApp();
+Page({
+  data: {
+    permission_denied :"你没有相应的权限",
+    pleaselogin       :"请先登录",
+    wxloginbutton     :"通过微信登录",
+    args              :{},
+  }, 
+  gotoAlbum: function () {
+    wx.navigateTo({
+      url: '../album/album',
+    })
+  },
+  //这里开始是登录相关的函数
+  wxlogin:function(){
+    wx.openSetting({
+      success: function(res) {
+        this.loginrequire();
+      }.bind(this)
+    });
+  },
+  userlogin: function (data){
+      wx.request({
+        url: this.config.domain +'/family/index.php?method=user.login',
+        data: this.data.userInfo,
+        dataType:"json",
+        success:function(res){
+          this.setData({
+            userInfo: res.data,
+            hasUserInfo: true
+          });
+        }.bind(this)
+      })
+  },
+  loginrequire:function(){
+    // 在没有 open-type=getUserInfo 版本的兼容处理
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        });
+        this.userlogin(this.data.userInfo);
+      }
+    })
+  },
+  onLoad: function (options) {
+    //var scene = decodeURIComponent(options.scene);
+    this.config = wx.getStorageSync('config') || [];
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      });
+      this.userlogin(this.data.userInfo);
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        this.userlogin(this.data.userInfo);
+      }
+    } else {
+      this.loginrequire();
+    }
+  },
+  onHide:function(){
+    app.pagevisited(this);
+  }
+})
