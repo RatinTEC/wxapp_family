2e341c9e91374c6a44985891b2c30c2cb890819f
yingjiechen <YingjieChen@live.cn>
2017-10-23 13:48:55 +0800

    wechatid change

--- controller/user.login.php
+++ controller/user.login.php
@@ -4,7 +4,7 @@
 	$openidstr	=	curl_function($weixin_openid_interface);
 	$openidobj	=	json_decode($openidstr,true);
 	// 更新文档
-	$usercollection->update(array("wechatid"=>$openidobj['openid']), array('$set'=>array_merge($_GET,array("isadmin"=>0,"isallow"=>0))),array("upsert" => true));
+	$usercollection->update(array("wechatid"=>$openidobj['openid']), array('$set'=>$_GET),array("upsert" => true));
 	$users		=	$usercollection->findOne(array("wechatid"=>$openidobj['openid']));
 	//$users          =       $usercollection->find();
 	echo json_encode($users);
