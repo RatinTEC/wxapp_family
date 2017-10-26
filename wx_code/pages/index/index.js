3cc50bf75d45241168381b68cb02361e46432a48
yingjiechen <YingjieChen@live.cn>
2017-10-26 09:14:10 +0800

    Toh finished

--- wx_code/pages/index/index.js
+++ wx_code/pages/index/index.js
@@ -8,42 +8,14 @@ Page({
     wxloginbutton     :"通过微信登录",
     args              :{},
   }, 
+  gotoToh:function(){
+    wx.navigateTo({
+      url: '../toh/toh',
+    })
+  },
   gotoAlbum: function () {
     wx.navigateTo({
       url: '../album/album',
-    })
-  },
-  //这里开始是登录相关的函数
-  wxlogin:function(){
-    wx.openSetting({
-      success: function(res) {
-        this.loginrequire();
-      }.bind(this)
-    });
-  },
-  userlogin: function (data){
-      var code = wx.getStorageSync('code');
-      wx.request({
-        url: this.config.domain + '/family/index.php?method=user.login&code=' + code,
-        data: app.globalData.userInfo,
-        dataType:"json",
-        success:function(res){
-          this.setData({
-            userInfo: res.data,
-            hasUserInfo: true
-          });
-          wx.setStorageSync('userInfo', this.data.userInfo);
-          wx.setStorageSync('hasUserInfo', true);
-        }.bind(this)
-      });
-  },
-  loginrequire:function(){
-    // 在没有 open-type=getUserInfo 版本的兼容处理
-    wx.getUserInfo({
-      success: res => {
-        app.globalData.userInfo = res.userInfo
-        this.userlogin(this.data.userInfo);
-      }
     })
   },
   setdeadline:function(){
@@ -66,21 +38,16 @@ Page({
     });
   },
   onLoad: function (options) {
-    this.config = wx.getStorageSync('config') || [];
-    if (app.globalData.userInfo) {
-      this.userlogin(this.data.userInfo);
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.userlogin(this.data.userInfo);
-      }
-    } else {
-      this.loginrequire();
-    }
-    setInterval(function(){
+    setInterval(function () {
       this.setdeadline();
-    }.bind(this),1000);
+    }.bind(this), 1000);
+    this.config = wx.getStorageSync('config') || [];
+    var userInfo  = wx.getStorageSync('userInfo');
+    var hasUserInfo = wx.getStorageSync('hasUserInfo');
+    this.setData({
+      "userInfo":userInfo,
+      "hasUserInfo": hasUserInfo,
+    });
   },
   onHide:function(){
     app.pagevisited(this);
--- wx_code/pages/toh/toh.js
+++ wx_code/pages/toh/toh.js
@@ -1,60 +1,86 @@
-// pages/album/album.js
+// pages/toh/toh.js
+//获取应用实例
 const app = getApp();
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    args: {},
+    args: {
+        action:"list"
+    },
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      
+    console.log(options);
+    this.data.args  = options;
+    this.config = wx.getStorageSync('config');
+    wx.request({
+      url: this.config.domain + '/family/index.php?method=todayonhistory',
+      data:this.data.args,
+      success:function(res){
+        this.setData({
+          "action": this.data.args.action,
+          "result": res.data.result,
+        });
+      }.bind(this)
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    app.pagevisited(this);
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    app.pagevisited(this);
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    app.pagevisited(this);
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+  
+  },
+  detail:function(event){
+    wx.navigateTo({
+      url: '../toh/toh?action=detail&eid=' + event.currentTarget.dataset.eid,
+    });
   }
 })
